395ef5f0bc296f422bb176949385a34dcb5b2a1d
Jacek <Jacek@DESKTOP-V5VJQ13>
2017-02-17 21:59:58 +0100

    dodanie id

--- QuickShopping/src/model/Category.java
+++ QuickShopping/src/model/Category.java
@@ -2,22 +2,36 @@ package model;
 
 public class Category {
 
+	private int id;
 	private int priority;
 	private String name;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
 
 	public int getPriority() {
 		return priority;
 	}
+
 	public void setPriority(int priority) {
 		this.priority = priority;
 	}
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
-	public Category(int priority, String name) {
+
+	public Category(int id, int priority, String name) {
+		this.id = id;
 		this.priority = priority;
 		this.name = name;
 	}
